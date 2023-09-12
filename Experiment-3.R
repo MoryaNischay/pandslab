5caad8092ba5c3221b332c342d5e93bc04f64f3c
Nischay Morya <93078084+MoryaNischay@users.noreply.github.com>
2023-09-12 10:58:43 +0530

    Update Experiment-3.R

--- Experiment-3.R
+++ Experiment-3.R
@@ -37,4 +37,21 @@ p<-dhyper(3,17,233,5) ##dhyper(a,m,n,k)
 print(paste("P(X=3) =",p))
 
 
-##Q5
+#Q5
+n<-31
+p=0.447
+#1 X follows a binomial distribution
+#2
+x_values <- 0:31
+pmf <- dbinom(x_values, n, p)
+barplot(pmf, names.arg = x_values, xlab = "Number of Students Using Wikipedia", ylab = "Probability", main = "Probability Mass Function(PMF) of X")
+#3
+cdf <- pbinom(x_values, size = n, prob = p)
+plot(x_values, cdf, type = "s", xlab = "Number of Students Using Wikipedia", ylab = "Cumulative Probability", main = "Cumulative Distribution Function (CDF) of X")
+#4
+mean_X <- n * p
+mean_X
+variance_X <- n * p * (1 - p)
+variance_X
+std_dev_X <- sqrt(variance_X)
+ std_dev_X
